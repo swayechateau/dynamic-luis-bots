d09fa1a06e876e1caf4d74e97f0258e41eedb769
Glitch (handsomely-barber) <none>
2018-03-21 04:04:44 +0000

    🐧🐾 Checkpoint ./server.js:702954/2503

--- server.js
+++ server.js
@@ -28,3 +28,70 @@ var bot = new builder.UniversalBot(connector, function (session) {
     
     session.send(respond);
 });
+
+var request = require('request');
+var debug = require('debug')('botkit:subscribe_events');
+
+module.exports = function(controller) {
+
+    debug('Subscribing to Cisco webhook events...');
+
+    var webhook_name = controller.config.webhook_name || 'Botkit Firehose';
+
+    var list = controller.api.webhooks.list().then(function(list) {
+        var hook_id = null;
+
+        for (var i = 0; i < list.items.length; i++) {
+            if (list.items[i].name == webhook_name) {
+                hook_id = list.items[i].id;
+            }
+        }
+
+        var hook_url = 'https://' + controller.config.public_address + '/ciscospark/receive';
+
+        debug('Cisco Spark: incoming webhook url is ', hook_url);
+
+        if (hook_id) {
+            controller.api.webhooks.update({
+                id: hook_id,
+                resource: 'all',
+                targetUrl: hook_url,
+                event: 'all',
+                secret: controller.config.secret,
+                name: webhook_name,
+            }).then(function(res) {
+                debug('Cisco Spark: SUCCESSFULLY UPDATED CISCO SPARK WEBHOOKS');
+            }).catch(function(err) {
+                debug('FAILED TO REGISTER WEBHOOK', err);
+                throw new Error(err);
+            });
+
+        } else {
+            controller.api.webhooks.create({
+                resource: 'all',
+                targetUrl: hook_url,
+                event: 'all',
+                secret: controller.config.secret,
+                name: webhook_name,
+            }).then(function(res) {
+
+                debug('Cisco Spark: SUCCESSFULLY REGISTERED CISCO SPARK WEBHOOKS');
+            }).catch(function(err) {
+                debug('FAILED TO REGISTER WEBHOOK', err);
+                throw new Error(err);
+            });
+
+        }
+    });
+};
+var controller = builder.sparkbot({
+    // debug: true,
+    limit_to_domain: ['dimensiondata.com'],
+    //limit_to_org: 'my_cisco_org_id',
+    public_address: process.env.public_address,
+    ciscospark_access_token: process.env.access_token,
+    studio_token: process.env.studio_token, // get one from studio.botkit.ai to enable content management, stats, message console and more
+    secret: process.env.secret, // this is an RECOMMENDED but optional setting that enables validation of incoming webhooks
+    webhook_name: 'Cisco Spark bot created with Botkit, override me before going to production',
+    studio_command_uri: process.env.studio_command_uri,
+});
