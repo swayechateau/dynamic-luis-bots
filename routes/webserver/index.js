1d56d23ab5ea3db32461950c009b1d2a800fc3a2
Glitch (handsomely-barber) <none>
2018-05-29 00:53:13 +0000

    Import of MercylessReap/bot-framework-node

--- routes/webserver/index.js
+++ routes/webserver/index.js
@@ -13,14 +13,16 @@ const express = require('express')
     , guide = require('./routes/guides/index');
 
     router.get('/',ensureAuthenticated,(req,res)=>{
-      api.getPermissions().then((perms)=>{
+      var user = api.getCurrentUser(req.user._id)
+      res.send(user)
+     /*   .then((perms)=>{
         perms.data.forEach((item,index)=>{
           if(item.userId === req.user._id){
             global.user = {id:req.user._id,name:req.user.name,perm:{admin:item.admin,wizard:item.wizard,department:item.department}}
           }
         })
       })
-        res.redirect('/dashboard')
+        res.redirect('/dashboard') */
     })
 	  //Auth
     router.use('/auth', auth);
